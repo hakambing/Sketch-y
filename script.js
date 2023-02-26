914c523609d7c4af299497e7a6e882569bf8dab7
hakambing <abdulhakamhasbullah@gmail.com>
2023-02-26 20:37:53 +0800

    Finished Toggle Grid Lines

--- script.js
+++ script.js
@@ -1,53 +1,100 @@
 pen_colour = "#2D2C2C";
-bg_colour = "#FFFFFF"
+penMode = "penMode";
+var lines = "lines";
 
+sketchBoard = document.querySelector("#sketchBoard");
 penColour = document.querySelector('#Pen');
-bgColour = document.querySelector('#Bg');
 // rainbow = document.getElementById('rainbow');
 // eraser = document.getElementById('eraser');
-// gridLines = document.getElementById('gridLines');
+gridLines = document.getElementById('gridLines');
 // gridSlider = document.getElementById('gridSlider');
 // clear = document.getElementById('clear');
 
+// function setPenMode(newMode) {
 
-console.log(penColour);
+//     penMode = newMode;
+//     console.log(penMode);
+// }
 
-function setupGrid(size) {
-    document.getElementById('sketchBoard').style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    document.getElementById('sketchBoard').style.gridTemplateRows = `repeat(${size}, 1fr)`
+setGridLines();
+function setGridLines() {
 
-    for (let i = 0; i < size * size; i++) {
-        gridElement = document.createElement('div');
-        gridElement.classList.add('grid-element')
-        gridElement.addEventListener('mouseover', changeColor)
-        gridElement.addEventListener('mousedown', changeColor)
-        sketchBoard.appendChild(gridElement)
-        gridElement.style.border = "1px solid black";
-        
+    gridLines.classList.toggle('yesLines');
+
+    if (gridLines.classList.contains("yesLines")) {
+        setupGrid(5, "lines");
+
+    } else {
+        setupGrid(5, "noLines");
     }
+    // console.log(lines);
+    // setupGrid(5,newLine);
 }
 
-penColour.addEventListener('input',()=>{
-    pen_colour = penColour.value;
-});
+// console.log(penColour);
 
-bgColour.addEventListener('input',()=>{
-    bg_colour = bgColour.value;
-    gridElement.style.background = bg_colour;
-});
+function setupGrid(size, lines) {
+    sketchBoard.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    sketchBoard.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    // console.log("gay" + lines);
+    let x = 0;
+    for (let i = 0; i < size * size; i++) {
+        // console.log("gay" + lines);
+
+        gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        boxes = document.querySelectorAll(".grid-element");
+
+        if (boxes !== null && x == 0) {
+            console.log("taik");
+            boxes.forEach(box => {
+                console.log("poop");
+                box.remove();
+                x++
+            });
+        }
+
+
+        // gridElement.addEventListener('mouseover', changeColor)
+        // gridElement.addEventListener('mousedown', changeColor)
+        if (lines == "lines") {
+            console.log("helo");
+            gridElement.style.border = "1px solid black";
+        } else if (lines == "noLines") {
+            console.log("bye");
+            gridElement.style.border = "0";
+        }
+
+        gridElement.style.background = "white";
+        sketchBoard.appendChild(gridElement);
+
+        // console.log(penMode);
+    }
+
+}
+
+// penColour.addEventListener('input', () => {
+//     pen_colour = penColour.value;
+// });
+
+// eraser.onclick = () => setPenMode('eraser');
+gridLines.onclick = () => setGridLines();
+
+
 
 function changeColor(e) {
-    if (e.type === 'mouseover' && !mousedown) return
-    // if (currentMode === 'rainbow') {
-    //   const randomR = Math.floor(Math.random() * 256)
-    //   const randomG = Math.floor(Math.random() * 256)
-    //   const randomB = Math.floor(Math.random() * 256)
-    //   e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
-    //} else 
+    // if (e.type === 'mouseover' && !mouseDown) return
+    // if (penMode === 'penMode') {
+    //     gridElement.style.backgroundColor = pen_colour;
+    // // } else if (penMode === 'eraserMode') {
+    // //     gridElement.style.backgroundColor = "#FFFFFF";
+    //  }
 
-    e.target.style.backgroundColor = pen_colour;
 
 }
 
-setupGrid(5);
+
+
+
+// setupGrid(5,lines);
 
