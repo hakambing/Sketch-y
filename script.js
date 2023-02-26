355119bc26126a03940be2493ece6e3ac495eed8
hakambing <abdulhakamhasbullah@gmail.com>
2023-02-26 21:48:29 +0800

    Finished Grid Slider

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
+
 pen_colour = "#2D2C2C";
 penMode = "penMode";
-var lines = "lines";
 
 sketchBoard = document.querySelector("#sketchBoard");
 penColour = document.querySelector('#Pen');
 // rainbow = document.getElementById('rainbow');
 // eraser = document.getElementById('eraser');
 gridLines = document.getElementById('gridLines');
-// gridSlider = document.getElementById('gridSlider');
+gridSlider = document.querySelector('#gridSliderRange');
 // clear = document.getElementById('clear');
 
 // function setPenMode(newMode) {
@@ -16,19 +16,15 @@ gridLines = document.getElementById('gridLines');
 //     console.log(penMode);
 // }
 
-setGridLines();
-function setGridLines() {
 
-    gridLines.classList.toggle('yesLines');
+function setGridLines(size) {
 
-    if (gridLines.classList.contains("yesLines")) {
-        setupGrid(5, "lines");
+    if (gridLines.classList.contains("noLines")) {
+        setupGrid(size, "noLines");
 
     } else {
-        setupGrid(5, "noLines");
+        setupGrid(size, "lines");
     }
-    // console.log(lines);
-    // setupGrid(5,newLine);
 }
 
 // console.log(penColour);
@@ -36,21 +32,17 @@ function setGridLines() {
 function setupGrid(size, lines) {
     sketchBoard.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     sketchBoard.style.gridTemplateRows = `repeat(${size}, 1fr)`
-    // console.log("gay" + lines);
-    let x = 0;
+    let j = 0;
     for (let i = 0; i < size * size; i++) {
-        // console.log("gay" + lines);
-
         gridElement = document.createElement('div');
         gridElement.classList.add('grid-element');
         boxes = document.querySelectorAll(".grid-element");
 
-        if (boxes !== null && x == 0) {
-            console.log("taik");
+        //code for deleting existing grid
+        if (boxes !== null && j == 0) {
+            j++;
             boxes.forEach(box => {
-                console.log("poop");
                 box.remove();
-                x++
             });
         }
 
@@ -58,10 +50,8 @@ function setupGrid(size, lines) {
         // gridElement.addEventListener('mouseover', changeColor)
         // gridElement.addEventListener('mousedown', changeColor)
         if (lines == "lines") {
-            console.log("helo");
             gridElement.style.border = "1px solid black";
         } else if (lines == "noLines") {
-            console.log("bye");
             gridElement.style.border = "0";
         }
 
@@ -78,7 +68,11 @@ function setupGrid(size, lines) {
 // });
 
 // eraser.onclick = () => setPenMode('eraser');
-gridLines.onclick = () => setGridLines();
+gridLines.onclick = () => setGridLines(gridSlider.value,gridLines.classList.toggle('noLines'));
+gridSlider.addEventListener('input', function() {
+    setGridLines(gridSlider.value);
+    document.getElementById("range-value").innerHTML = gridSlider.value + " x " + gridSlider.value;
+})
 
 
 
@@ -95,6 +89,8 @@ function changeColor(e) {
 
 
 
-
-// setupGrid(5,lines);
+window.addEventListener("load", (event) => {
+    gridSlider.value = 16;
+    setGridLines(gridSlider.value);
+})
 
